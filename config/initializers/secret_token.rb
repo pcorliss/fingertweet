b5d3c7279fc53882008041e4e3120ba0a22c3077
Philip Corliss <pcorliss@50projects.com>
2014-01-04 11:26:38 -0600

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FingerTweet::Application.config.secret_key_base = 'd372de587c128c134e16382918f36f956bf1fe936d283119e27ffed28a0fab98b1fb12224adee8660d98e949e37d0f51adf680c2a87a3511ce73fce224d8f9c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FingerTweet::Application.config.session_store :cookie_store, key: '_FingerTweet_session'
