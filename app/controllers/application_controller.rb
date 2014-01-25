c63fb3eb1ff97333b45ab15881fed535000f7a23
Philip Corliss <pcorliss@50projects.com>
2014-01-25 10:17:13 -0600

    Adding last indexed footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_last_index
+
+  def set_last_index
+    @last_index = Rails.cache.read('last_tweet_index')
+  end
 end
